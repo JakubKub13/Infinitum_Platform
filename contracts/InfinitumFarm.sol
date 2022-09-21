89cf48ebf67ed7bb8fe22674ae00d9f1ce1f31bf
JK1331 <jakubkubala3@gmail.com>
2022-09-21 10:06:38 +0200

    Adding contract dependencies

---
+++ contracts/InfinitumFarm.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+//pragma solidity 0.8.8;
+
+//import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+//import "./InfinitumToken.sol";
+//import "./InfinitasFactory.sol";
+//import "./Lottery.sol";
