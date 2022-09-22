d711f6299666afd6a6d4ba571393ddc169d0f016
JK1331 <jakubkubala3@gmail.com>
2022-09-22 11:08:01 +0200

    Setting up lottery test

--- test/Lottery.test.ts
+++ test/Lottery.test.ts
@@ -12,4 +12,23 @@ describe("Lottery contract", function () {
     let infinitasFactory: Contract;
     let infinitumToken: Contract;
     let mockLinkToken: Contract;
+
+    beforeEach(async () => {
+        const Lottery = await ethers.getContractFactory("MockLottery");
+        const InfinitasFactory = await ethers.getContractFactory("InfinitasFactory");
+        const InfinitumToken = await ethers.getContractFactory("InfinitumToken");
+        const MockLinkToken = await ethers.getContractFactory("MockERC20");
+        mockLinkToken = await MockLinkToken.deploy("MockLink", "mLINK");
+        infinitasFactory = await InfinitasFactory.deploy();
+        infinitumToken = await InfinitumToken.deploy();
+        [owner, jacob, martin] = await ethers.getSigners();
+        await mockLinkToken.mint(owner.address, ethers.utils.parseEther("9999"));
+
+        let lotteryParams = [
+            // infinitasFactory.address,
+            infinitumToken.address,
+            mockLinkToken.address,
+            
+        ]
+    })
 })
