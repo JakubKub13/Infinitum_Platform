b5a31443ef652656a518b9f7465cada783a6c020
JK1331 <jakubkubala3@gmail.com>
2022-09-23 09:27:16 +0200

    Deploy script INFS factory

--- deploy/02-deploy-infinitas-factory.ts
+++ deploy/02-deploy-infinitas-factory.ts
@@ -4,27 +4,26 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { NetworkConfig, developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } from "../helper-hardhat-config";
 import verify from "../verify";
 
-const deployINFTtoken: DeployFunction = async function (
+const deployINFSfactory: DeployFunction = async function (
     hre: HardhatRuntimeEnvironment
 ) {
     const { deployments, getNamedAccounts, network, ethers } = hre;
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
-    
-    log("Deploying Infinitum token.......");
-    await deploy("InfinitumToken", {
+
+    log("Deploying Infinitas factory.......");
+    await deploy("InfinitasFactory", {
         from: deployer,
         log: true,
         args: []
     })
-    log("Infinitum token Deployed!");
+    log("Infinitas factory Deployed!");
     log("----------------------------------------------");
     log("You are deploying to a local network, you will need a local network running to interact");
     log("Please run npx hardhat console --network localhost to interact with deployed smart contracts");
     log("-----------------------------------------------");
 }
 
-export default deployINFTtoken
-deployINFTtoken.tags = ["all", "token"]
-    
+export default deployINFSfactory
+deployINFSfactory.tags = ["all", "factory"]
