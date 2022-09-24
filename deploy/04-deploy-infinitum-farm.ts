fc514f39ea5a21da85d5216476e3a13998bde51a
JK1331 <jakubkubala3@gmail.com>
2022-09-24 11:07:51 +0200

    Preparing farm deploy script

--- deploy/04-deploy-infinitum-farm.ts
+++ deploy/04-deploy-infinitum-farm.ts
@@ -3,3 +3,7 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { NetworkConfig, developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } from "../helper-hardhat-config";
 import verify from "../verify";
+
+const deployInfinitumFarm: DeployFunction = async function (
+    hre: HardhatRuntimeEnvironment
+) {}
