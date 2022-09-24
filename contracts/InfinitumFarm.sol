8a713c4a8fbdd4901cb57fad28296e8728161b0a
JK1331 <jakubkubala3@gmail.com>
2022-09-24 09:08:11 +0200

    Init Farm state variables

--- contracts/InfinitumFarm.sol
+++ contracts/InfinitumFarm.sol
@@ -13,4 +13,25 @@ import "./Lottery.sol";
 ///      Functions calculateYield time and calculateYieldTotal are used to calculate all necessary yield metrics.
 ///      Ownership of the InfinitumToken contract should be transferred to the InfinitumFarm contract immediately after deployment.
 
-contract InfinitumFarm {}
+contract InfinitumFarm {
+
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public isStaking;
+    mapping(address => uint256) public startTime;
+    mapping(address => uint256) public inftBalance;
+    mapping(string => uint256) public nftCount;
+
+    string public name = "Infinitum Farm";
+
+    IERC20 public daiStablecoin;
+    InfinitumToken public infinitumToken;
+    InfinitasFactory public infinitasFactory;
+    Lottery public lottery;
+
+    uint256 private nftPrice;
+
+    event Stake(address indexed from, uint256 amount);
+    event Unstake(address indexed from, uint256 amount);
+    event YieldWithdraw(address indexed to, uint256 amount);
+    event MintNFT(address indexed to, uint256 indexed tokenId);
+}
