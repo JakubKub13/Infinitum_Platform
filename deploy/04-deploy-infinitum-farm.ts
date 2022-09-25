72501042fa6dcf6f56e0df4248ccbfed9e8eaa80
JK1331 <jakubkubala3@gmail.com>
2022-09-25 19:39:13 +0200

    Deploy script token farm

--- deploy/04-deploy-infinitum-farm.ts
+++ deploy/04-deploy-infinitum-farm.ts
@@ -1,7 +1,9 @@
+import { Contract } from "ethers";
 import { getNamedAccounts, deployments, network, run } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { NetworkConfig, developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } from "../helper-hardhat-config";
+import { InfinitasFactory, InfinitumToken ,Lottery,   } from "../typechain-types"
 import verify from "../verify";
 
 const deployInfinitumFarm: DeployFunction = async function (
@@ -11,4 +13,14 @@ const deployInfinitumFarm: DeployFunction = async function (
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
+    let daiStablecoin: Contract;
+    let infinitumToken: InfinitumToken;
+    let infinitasFactory: InfinitasFactory;
+    let lottery: Lottery;
+
+    if(chainId == 31337) {
+        
+    }
+
+
 }
